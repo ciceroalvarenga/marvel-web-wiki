cee164918db67b596626e7362f357279904d8243
Cícero Alvarenga Santos Neto <ciceroalvarenga2017@hotmail.com>
2021-06-18 21:49:52 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import md5 from 'md5';
-const PRIVATE_KEY = 'e773b5e727aa4ae930fed3bcc8eb6753692f2240';
-const PUBLIC_KEY = '335482a5ab9180863c1b9398c3616015';
+const PRIVATE_KEY = '';
+const PUBLIC_KEY = '';
 
 const time = Number(new Date());
 
